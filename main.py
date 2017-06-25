3e1f5f1aec649d0a664023b4eaa2d2521103a224
GA1 <nvkga1@gmail.com>
2017-06-25 22:07:35 +0200

    Use png for authors

--- main.py
+++ main.py
@@ -2,32 +2,33 @@ from PIL import Image
 import os
 
 
-
-
 def resize_as_epic(img):
     old_size = img.size
     old_width = old_size[0]
     old_height = old_size[1]
-    new_width = 1000
-    new_height = (int)(old_height * 1000 / old_width)
-    return img.resize((new_width, new_height), Image.ANTIALIAS)
+    new_width = 800
+    new_height = (int)(old_height * 800 / old_width)
+    if old_width < new_width:
+        return img
+    else:
+        return img.resize((new_width, new_height), Image.ANTIALIAS)
 
 
 def resize_as_thumbnail(img):
-    return img.resize((140, 140), Image.ANTIALIAS)
+    return img.resize((160, 160), Image.ANTIALIAS)
 
 
 def resize_as_author(img):
-    return img.resize((140, 140), Image.ANTIALIAS)
+    return img.resize((200, 200), Image.ANTIALIAS)
 
 
 files_names = os.listdir('./images')
 for file_name in files_names:
     img = Image.open('images/' + file_name)
-    if file_name[-8:-4] == 'epic':
+    if 'epic' in file_name:
         img = resize_as_epic(img)
-    elif file_name[-13:-4] == 'thumbnail':
+    elif 'thumbnail' in file_name:
         img = resize_as_thumbnail(img)
     else:
         img = resize_as_author(img)
-    img.save('results/' + file_name)
+    img.save('results/' + file_name.replace('_raw', ''))
